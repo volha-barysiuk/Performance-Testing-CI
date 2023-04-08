e4beb356fecc527c241de63b96e791399c03f884
volha-barysiuk <54316767+volha-barysiuk@users.noreply.github.com>
2023-04-08 21:25:45 +0400

    Update ShopizerPerfTest.scala

--- Gatling/src/test/scala/simulations/ShopizerPerfTest.scala
+++ Gatling/src/test/scala/simulations/ShopizerPerfTest.scala
@@ -13,9 +13,9 @@ class ShopizerPerfTest extends Simulation{
    setUp(
       scnProceedToCheckout.inject(
         nothingFor(5),
-        rampUsersPerSec(System.getProperty("rampupFrom", "1").toInt) to (System.getProperty("rampupTo", "20").toInt) during (System.getProperty("rampupDuration", "1").toInt minutes),
-        constantUsersPerSec(System.getProperty("constantUsers", "20").toInt) during (System.getProperty("constantUsersDuration", "1").toInt minutes),
-        rampUsersPerSec(System.getProperty("rampdownFrom", "20").toInt) to (System.getProperty("rampdownTo", "1").toInt) during (System.getProperty("rampdownDuration", "1").toInt minutes)
+        rampUsers(System.getProperty("rampupUsers1", "20").toInt).during(System.getProperty("rampupDuration", "3").toInt minutes),
+        rampUsers(System.getProperty("rampupUsers2", "40").toInt).during(System.getProperty("rampupDuration", "3").toInt minutes),
+        rampUsers(System.getProperty("rampupUsers3", "50").toInt).during(System.getProperty("rampupDuration", "1").toInt minutes)
       ).protocols(httpProtocol)
    )
 
