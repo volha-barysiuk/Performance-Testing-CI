250adaefc972f4478314ef4852c87da16f31ec2f
volha-barysiuk <54316767+volha-barysiuk@users.noreply.github.com>
2023-04-08 16:41:12 -0400

    Update ShopizerPerfTest.scala

--- Gatling/src/test/scala/simulations/ShopizerPerfTest.scala
+++ Gatling/src/test/scala/simulations/ShopizerPerfTest.scala
@@ -12,7 +12,7 @@ class ShopizerPerfTest extends Simulation{
 
    setUp(
       scnProceedToCheckout.inject(
-         rampConcurrentUsers(5).to(100).during(30.minutes)
+         rampConcurrentUsers(20).to(300).during(10.minutes)
       ).protocols(httpProtocol)
    )
 
