178aae76ea9d6520af446ee49ef37c22859bf1ea
volha-barysiuk <54316767+volha-barysiuk@users.noreply.github.com>
2023-04-08 18:42:36 +0400

    Update ShopizerPerfTest.scala

--- Gatling/src/test/scala/simulations/ShopizerPerfTest.scala
+++ Gatling/src/test/scala/simulations/ShopizerPerfTest.scala
@@ -4,13 +4,20 @@ import config.BaseHelpers._
 import io.gatling.core.Predef._
 import scenarios.AddProductToCart._
 
+import scala.concurrent.duration.DurationInt
+
 
 class ShopizerPerfTest extends Simulation{
 
 
    setUp(
-      scnProceedToCheckout.inject(atOnceUsers(System.getProperty("ShopizerUsers", "1").toInt))
-    ).protocols(httpProtocol)
+      scnProceedToCheckout.inject(
+        nothingFor(5),
+        rampUsersPerSec(System.getProperty("rampupFrom", "1").toInt) to (System.getProperty("rampupTo", "20").toInt) during (System.getProperty("rampupDuration", "1").toInt minutes),
+        constantUsersPerSec(System.getProperty("constantUsers", "20").toInt) during (System.getProperty("constantUsersDuration", "1").toInt minutes),
+        rampUsersPerSec(System.getProperty("rampdownFrom", "20").toInt) to (System.getProperty("rampdownTo", "1").toInt) during (System.getProperty("rampdownDuration", "1").toInt minutes)
+      ).protocols(httpProtocol)
+   )
 
 
 }
