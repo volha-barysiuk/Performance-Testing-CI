306bb958ef730a7547e70da69b9472399a7faeec
volha-barysiuk <54316767+volha-barysiuk@users.noreply.github.com>
2023-04-14 05:02:19 -0400

    Update ShopizerPerfTest.scala

--- Gatling/src/test/scala/simulations/ShopizerPerfTest.scala
+++ Gatling/src/test/scala/simulations/ShopizerPerfTest.scala
@@ -13,7 +13,7 @@ class ShopizerPerfTest extends Simulation{
    setUp(
       scnProceedToCheckout.inject(
          rampConcurrentUsers(System.getProperty("minUsers", "1").toInt).to(System.getProperty("maxUsers", "20").toInt).during(System.getProperty("rampTime", "1").toInt.minutes),
-         //constantConcurrentUsers(System.getProperty("maxUsers", "20").toInt).during(System.getProperty("constTime", "1").toInt.minutes)
+         constantConcurrentUsers(System.getProperty("maxUsers", "20").toInt).during(System.getProperty("constTime", "1").toInt.minutes)
       ).protocols(httpProtocol)
    )
 
